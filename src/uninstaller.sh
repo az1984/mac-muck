490311982fd78f4fd85f731ff252dab3ad0c8d08
az1984 <6044472+az1984@users.noreply.github.com>
2026-04-10 22:50:02 -0400

    Update uninstaller.sh

--- src/uninstaller.sh
+++ src/uninstaller.sh
@@ -903,8 +903,18 @@ RemovePathForUsers() {
   local users=()
   while (( $# )); do
     case "$1" in
-      --tolerant-missing) tolerant=true ;;
-      --needs-root)       needs_root=true ;;
+      --tolerant-missing)
+        if [[ $tolerant == true ]]; then
+          echo "${my_echo_prefix}${my_err_prefix} Bad input: duplicate --tolerant-missing flag."
+          return 2
+        fi
+        tolerant=true ;;
+      --needs-root)
+        if [[ $needs_root == true ]]; then
+          echo "${my_echo_prefix}${my_err_prefix} Bad input: duplicate --needs-root flag."
+          return 2
+        fi
+        needs_root=true ;;
       --*) echo "${my_echo_prefix}${my_err_prefix} Unknown flag: $1"; return 2 ;;
       *) users+=("$1") ;;
     esac
